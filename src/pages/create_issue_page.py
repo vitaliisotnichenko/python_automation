ef0ec962969182653d4324281a31d3ad6cc2d1cd
vitaliisotnichenko <vitalii.sotnichenko@gmail.com>
2019-12-05 08:31:33 +0200

    Update create_issue page

--- src/pages/create_issue_page.py
+++ src/pages/create_issue_page.py
@@ -1,6 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import time
 from .base_page import BasePage
@@ -8,20 +8,29 @@ from .base_page import BasePage
 class CreateIssue(BasePage):
 
     def should_have_title(self):
-        __create_issue_title = self.browser.find_element(By.CSS_SELECTOR, "[title=\"Create Issue\"]").text
-        return "Create Issue" in __create_issue_title
+        for i in range(3):
+            try:
+                __create_issue_title = self.browser.find_element(By.CSS_SELECTOR, "[title=\"Create Issue\"]").text
+                if "Create Issue" in __create_issue_title:
+                    break
+            except (NoSuchElementException, StaleElementReferenceException):
+                    time.sleep(5)
+                    i += 1
 
-    def choose_the_project(self):
-        __select = Select.find_element(By.CSS_SELECTOR, "#project")
-        __select.select_by_visible_text("Webinar (WEBINAR")
+    def choose_the_project(self, project_name):
+         #type: (WebDriver) -> ()
+        __project_field = self.browser.find_element(By.CSS_SELECTOR, "#project-field")
+        __project_field.clear()
+        __project_field.send_keys(project_name)
+        __project_field.send_keys(Keys.ENTER)
 
     def click_create_issue_button(self):
         for i in range(3):
             try:
                 __create_issue_button = self.browser.find_element(By.CSS_SELECTOR, "#create_link")
-                __create_issue_button.click()
                 if __create_issue_button.is_displayed():
-                    break
+                   return __create_issue_button.click()
+
             except (NoSuchElementException, StaleElementReferenceException):
                 time.sleep(5)
                 i += 1
@@ -40,9 +49,12 @@ class CreateIssue(BasePage):
         __reporter_field.send_keys("webinar5")
         __reporter_field.send_keys(Keys.ENTER)
 
-    # def click_create_issue_button(self):
-    #     __create_issue_button = self.browser.find_element(By.CSS_SELECTOR, "#create-issue-submit").click()
-
     def is_alert_present(self):
-        __issue = self.browser.find_element_by_css_selector(".aui-will-close").text
-        return "Create Issue" in __issue
+        for i in range(3):
+            try:
+                __issue = self.browser.find_element_by_css_selector(".aui-will-close")
+                if __issue.is_displayed():
+                    return str(__issue).text
+            except (NoSuchElementException, StaleElementReferenceException):
+                time.sleep(5)
+                i += 1
--- tests/UI/JiraLoginUITest.py
+++ tests/UI/JiraLoginUITest.py
@@ -1,6 +1,5 @@
 import time
 
-from src.pages import login_page
 from src.pages.login_page import LoginPage
 from src.pages.create_issue_page import CreateIssue
 from src.pages.main_page import MainPage
@@ -9,17 +8,17 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 
 class TestJiraLoginUI:
 
-    # def test_login_to_jira(self, browser):
-    #     type: # (WebDriver) -> ()
-        # self.login_page = LoginPage(browser)
-        # self.login_page.open()
-        # assert self.login_page.at_page()
-        # self.login_page.login_to_jira_enter_username("webinar5")
-        # self.login_page.login_to_jira_enter_password("webinar5")
-        # self.login_page.click_login_button()
-        # self.main_page = MainPage(browser)
-        # self.main_page.is_assigned_to_me_section()
-        # assert self.main_page.is_assigned_to_me_section()
+    def test_login_to_jira(self, browser):
+        # type: (WebDriver) -> ()
+        self.login_page = LoginPage(browser)
+        self.login_page.open()
+        assert self.login_page.at_page()
+        self.login_page.login_to_jira_enter_username("webinar5")
+        self.login_page.login_to_jira_enter_password("webinar5")
+        self.login_page.click_login_button()
+        self.main_page = MainPage(browser)
+        self.main_page.is_assigned_to_me_section()
+        assert self.main_page.is_assigned_to_me_section()
 
 
     def test_create_issue_in_jira(self, browser):
@@ -34,10 +33,10 @@ class TestJiraLoginUI:
         self.create_issue_page = CreateIssue(browser)
         self.create_issue_page.click_create_issue_button()
         self.create_issue_page.should_have_title()
-        self.create_issue_page.choose_the_project()
+        self.create_issue_page.choose_the_project("Webinar WEBINAR")
         self.create_issue_page.enter_summary_field()
         self.create_issue_page.enter_reporter()
         self.create_issue_page.click_create_issue_button()
-        assert self.create_issue_page.is_alert_present()
+        assert "Issue" in self.create_issue_page.is_alert_present()
 
 
