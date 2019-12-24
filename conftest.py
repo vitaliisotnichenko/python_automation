d99788a4c97b51ec4b2c05100e76d7c304026101
vitaliisotnichenko <vitalii.sotnichenko@gmail.com>
2019-12-24 12:45:17 +0200

    Add selenium grid

--- conftest.py
+++ conftest.py
@@ -4,7 +4,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def browser():
-    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+#     browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    grid_url = "http://localhost:4444/wd/hub"
+    desired_caps = DesiredCapabilities.CHROME
+    browser = webdriver.Remote(desired_capabilities=desired_caps, command_executor=grid_url)
     yield browser
     browser.quit()
 
