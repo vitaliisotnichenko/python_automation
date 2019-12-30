6cb1cd1460b8a5ef9bd80ac37a893336c7c1cbd2
vitaliisotnichenko <vitalii.sotnichenko@gmail.com>
2019-12-30 11:22:12 +0200

    Update Jenkinsfile

--- conftest.py
+++ conftest.py
@@ -2,12 +2,6 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-# @pytest.fixture(scope="function")
-# def browser():
-#     browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-#     yield browser
-#     browser.quit()
-
 @pytest.fixture(scope="function")
 def browser():
     grid_url = "http://localhost:4444/wd/hub"
