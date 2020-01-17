66f15aeeb079f3a7febb2a6c812351c113b5c15f
vitaliisotnichenko <vitalii.sotnichenko@gmail.com>
2020-01-18 00:38:50 +0200

    Update api tests

--- global_scope.py
+++ global_scope.py
@@ -1,6 +1,6 @@
 global_user = 'webinar5'
 global_pass = 'webinar5'
-url_api = "http://jira.hillel.it:8080"
+url_api = "https://jira.hillel.it"
 url_ui = "http://jira.hillel.it:8080/"
 headers = {
     'Content-Type': "application/json",
